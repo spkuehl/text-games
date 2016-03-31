3140402db2ab661fe9e8fea8fc4a73669d8283f7
spkuehl <skuehlhorn@gmail.com>
2016-03-31 10:44:58 -0500

    started the barebones templating. urls for home page and coming soon page

--- text_games/urls.py
+++ text_games/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$','text_games.views.home', name='home'),
+    url(r'^comingsoon/$', 'text_games.views.coming_soon', name='comingsoon')
 ]
