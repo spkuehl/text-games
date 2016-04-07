1bf7b1d01c089eaa524ee856c573081ab768494d
Carter Konz <carter.konz@gmail.com>
2016-04-07 18:25:24 -0500

    Think we finally fixed the database switch thing between Heroku / SQLite3

--- text_games/settings.py
+++ text_games/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'text_games.wsgi.application'
 #     }
 
 DATABASES = {}
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
