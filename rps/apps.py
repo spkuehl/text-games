718f7d4dc6fe0b331b07a38d83de42d165237b8a
Carter Konz <carter.konz@gmail.com>
2016-04-21 17:25:38 -0500

    Last time I was lying...

--- rps/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RpsConfig(AppConfig):
-    name = 'rps'
--- text_games/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render  # render a template to browser
-
-def home(request):
-    return render(request, "home.html")
-
-def coming_soon(request):
-    return render(request, "coming_soon.html")
